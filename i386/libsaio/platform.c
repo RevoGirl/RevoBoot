e47c5455dac9f5f8ec4002d0a288feac711ba7e8
Samantha <dutchhockeygoalie@yahoo.com>
2011-06-10 21:35:50 +0200

    Please do not wipe my data <i>after</i> I initialized it

--- i386/libsaio/platform.c
+++ i386/libsaio/platform.c
@@ -117,6 +117,16 @@ void initPlatform(int biosDevice)
 
 	_PLATFORM_DEBUG_DUMP("Booting with: %s\n", gPlatform.RevolutionVersionInfo);
 
+#if AUTOMATIC_SSDT_PR_CREATION || DEBUG_TURBO_RATIOS
+        int cpu = 0; 
+        
+        // Blank CPU core ratio limits.
+        for (; cpu < 6; cpu++)
+        {
+                gPlatform.CPU.CoreTurboRatio[cpu] = 0; // Gets updated in: i386/libsaio/Intel/cpu.c
+        }
+#endif
+
 	initCPUStruct();
 
 	/*
@@ -227,16 +237,6 @@ void initPlatform(int biosDevice)
 	}
 
 	_PLATFORM_DEBUG_DUMP("Static data for %d RAM BANKS used.\n", gPlatform.RAM.SlotCount);
-#endif
-
-#if AUTOMATIC_SSDT_PR_CREATION || DEBUG_TURBO_RATIOS
-	int cpu = 0;
-
-	// Blank CPU core ratio limits.
-	for (; cpu < 6; cpu++)
-	{
-		gPlatform.CPU.CoreTurboRatio[cpu] = 0; // Gets updated in: i386/libsaio/Intel/cpu.c
-	}
 #endif
 
 	_PLATFORM_DEBUG_SLEEP(15);
