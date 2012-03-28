a3d7fa649930a7e2271cc33d00098d00f7359d5f
Samantha <dutchhockeygoalie@yahoo.com>
2012-03-28 04:19:25 +0300

    Adding debug mode for testing.

--- i386/boot2/boot2.s
+++ i386/boot2/boot2.s
@@ -38,6 +38,8 @@
 #include <architecture/i386/asm_help.h>
 #include "memory.h"
 
+#define DEBUG   0           // Set to 0 by default. Use 1 for testing only!
+
 #define data32  .byte 0x66
 #define retf    .byte 0xcb
 
@@ -67,6 +69,28 @@ LABEL(boot2)                    # Entry point at 0:BOOTER_ADDR (will be called b
     pushl   %edi
     push    %ds                 # Save DS, ES
     push    %es
+
+#if DEBUG
+#-------------------------------------------------------------------------------
+# Writes an "R" to the console (flashing _ cursor) and waits for a key press.
+#
+#
+    push    %ax
+    push    %bx
+
+    mov        $0x0e52, %ax        # AH=0x0e (function code), AL=0x52 (character to print: 'R') 
+    mov     $0x0001, %bx        # BH=0x00 (page number), BL=0x01 (blue in graphics mode)
+    int     $0x10               # Display byte in teletype mode
+#
+# Wait for a key press.
+#
+    mov     $0x00,   %ah
+    int	    $0x16
+
+    pop     %bx
+    pop     %ax
+#-------------------------------------------------------------------------------
+#endif
 
     mov     %cs, %ax            # Update segment registers.
     mov     %ax, %ds            # Set DS and ES to match CS
