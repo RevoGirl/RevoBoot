09c0f04a51fe8adc5ccecc619c4325abb9a84688
Samantha <dutchhockeygoalie@yahoo.com>
2012-05-10 13:35:42 +0300

    Fixing the stupid Github whitespace errors.

--- i386/libsaio/platform.h
+++ i386/libsaio/platform.h
@@ -14,7 +14,7 @@
 //------------------------------------------------------------------------------
 
 #define	SNOW_LEOPARD			1	// Snow Leopard.
-#define LION					2	// Lion (the default).
+#define LION				2	// Lion (the default).
 #define MOUNTAIN_LION			6	// Mountain Lion (includes Lion changes).
 
 //------------------------------------------------------------------------------
@@ -25,7 +25,7 @@
 
 //------------------------------------------------------------------------------
 
-#define SMB_MEM_BANK_EMPTY      1	// For empty slots.
+#define SMB_MEM_BANK_EMPTY		1			// For empty slots.
 #define SMB_MEM_SIZE_1GB		1024
 #define SMB_MEM_SIZE_2GB		(SMB_MEM_SIZE_1GB * 2)
 #define SMB_MEM_SIZE_4GB		(SMB_MEM_SIZE_1GB * 4)
@@ -49,9 +49,9 @@
 
 //------------------------------------------------------------------------------
 
-#define EFI_SMBIOS_TABLE_GUID	{ 0xeb9d2d31, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
+#define EFI_SMBIOS_TABLE_GUID		{ 0xeb9d2d31, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
 
-#define EFI_ACPI_20_TABLE_GUID	{ 0x8868e871, 0xe4f1, 0x11d3, { 0xbc, 0x22, 0x0, 0x80, 0xc7, 0x3c, 0x88, 0x81 } }
+#define EFI_ACPI_20_TABLE_GUID		{ 0x8868e871, 0xe4f1, 0x11d3, { 0xbc, 0x22, 0x0, 0x80, 0xc7, 0x3c, 0x88, 0x81 } }
 
 #if INCLUDE_MPS_TABLE
 	#define EFI_MPS_TABLE_GUID	{ 0xeb9d2d2f, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
@@ -63,19 +63,19 @@
 #ifndef STATIC_RAM_SLOTS
 	#define MAX_SLOTS	4
 #else
-	#define MAX_SLOTS	STATIC_RAM_SLOTS // Defined in config/settings.h
+	#define MAX_SLOTS	STATIC_RAM_SLOTS		// Defined in config/settings.h
 #endif
 
 //==============================================================================
 
 typedef struct _RamSlotInfo_t
 {
-	uint32_t		Size;					// Module size in MB
-	uint32_t		Frequency;				// Frequency in Mhz.
-	const char *	Vendor;
-	const char *	PartNumber;
-	const char *	SerialNumber;
-	char *			spd;					// SPD Dump
+	uint32_t		Size;				// Module size in MB
+	uint32_t		Frequency;			// Frequency in Mhz.
+	const char *		Vendor;
+	const char *		PartNumber;
+	const char *		SerialNumber;
+	char *			spd;				// SPD Dump
 	bool			InUse;
 	uint8_t			Type;
 	uint8_t			BankConnections;		// Table type 6, see (3.3.7)
@@ -87,72 +87,72 @@ typedef struct _RamSlotInfo_t
 
 typedef struct _PlatformInfo_t
 {
-	cpu_type_t			ArchCPUType;				// Replacement for gArchCPUType
+	cpu_type_t			ArchCPUType;		// Replacement for gArchCPUType
 
-	int					AddressWidth;
+	int				AddressWidth;
 
-	int					Type;						// Initialized in platform.c to 2 for MacBook [Pro/Air]  
-													// or 1 for desktop PC's like: MacPro, Mac mini and iMac.
+	int				Type;			// Initialized in platform.c to 2 for MacBook [Pro/Air]  
+								// or 1 for desktop PC's like: MacPro, Mac mini and iMac.
 
-	long				LastKernelAddr;				// Used in AllocateKernelMemory
+	long				LastKernelAddr;		// Used in AllocateKernelMemory
 	
-	char *				RevoBootVersionInfo;		// Introduced in Revolution v0.6.45
+	char *				RevoBootVersionInfo;	// Introduced in Revolution v0.6.45
 
-	int					OSType;						// Type of Operating System.
+	int				OSType;			// Type of Operating System.
 
-	char *				OSVersion;					// OS version initialized with "10.6" in platform.c and 
-													// later updated in boot.c with the actual version info.
+	char *				OSVersion;		// OS version initialized with "10.6" in platform.c and 
+								// later updated in boot.c with the actual version info.
 
-	char *				ModelID;					// Initialized in platform.c and used in boot.c
+	char *				ModelID;		// Initialized in platform.c and used in boot.c
 
-	char *				KextFileName;				// Initialized and used in drivers.c
-	char *				KextFileSpec;				// Initialized and used in drivers.c
-	char *				KextPlistSpec;				// Initialized and used in drivers.c
+	char *				KextFileName;		// Initialized and used in drivers.c
+	char *				KextFileSpec;		// Initialized and used in drivers.c
+	char *				KextPlistSpec;		// Initialized and used in drivers.c
 
-	char *				KernelCachePath;			// Initialized in platform.c and used in boot.c, driver.c
+	char *				KernelCachePath;	// Initialized in platform.c and used in boot.c, driver.c
 
 #if PRE_LINKED_KERNEL_SUPPORT
-	bool				KernelCacheSpecified;		// Set to indicate that a full path is specified.
+	bool				KernelCacheSpecified;	// Set to indicate that a full path is specified.
 #endif
 
-	int					BIOSDevice;					// Initialized in platform.c (formely know as gBIOSDev).
+	int				BIOSDevice;		// Initialized in platform.c (formely know as gBIOSDev).
 
-	BVRef				BootVolume;					// Initialized in disk.c
-	BVRef				BootPartitionChain;			// Initialized in sys.c
-	BVRef				RootVolume;					// Initialized in disk.c (used in sys.c).
+	BVRef				BootVolume;		// Initialized in disk.c
+	BVRef				BootPartitionChain;	// Initialized in sys.c
+	BVRef				RootVolume;		// Initialized in disk.c (used in sys.c).
 
-	bool				BootRecoveryHD;				//
+	bool				BootRecoveryHD;		//
 
-	uint32_t			allocatedVRAM;				// Amount of allocated graphics memory (UEFI-BIOS settings).
+	uint32_t			allocatedVRAM;		// Amount of allocated graphics memory (UEFI-BIOS settings).
 
-	struct ACPI										// Used in acpi_patcher.h
+	struct ACPI						// Used in acpi_patcher.h
 	{
-		uint8_t			Type;						// System type. Referring to FACP->PM_Profile.
+		uint8_t			Type;			// System type. Referring to FACP->PM_Profile.
 
 		EFI_GUID		Guid;
 
-		EFI_UINT		BaseAddress;				// Either 32 or 64-bit (depends on platform info).
+		EFI_UINT		BaseAddress;		// Either 32 or 64-bit (depends on platform info).
 	} ACPI;
 
 	// Copied from: chameleon/i386/libsaio/platform.h
 	struct CPU
 	{
-		bool		Mobile;					// Set to true (in cpu/dynamic_data.h) for Mobile CPU's.
-		uint16_t	Type;					// CPU type ('cpu-type') used in the SMBIOS patcher.
-		uint32_t	Features;				// CPU Features like MMX, SSE2, VT, MobileCPU
-		uint32_t	Vendor;					// Vendor
-		uint32_t	Signature;				// Signature
-		uint32_t	Stepping;				// Stepping
-		uint32_t	Model;					// Model
-		uint32_t	ExtModel;				// Extended Model
-		uint32_t	Family;					// Family
-		uint32_t	ExtFamily;				// Extended Family
-		uint32_t	NumCores;				// Number of cores per package
-		uint32_t	NumThreads;				// Number of threads per package
+		bool		Mobile;				// Set to true (in cpu/dynamic_data.h) for Mobile CPU's.
+		uint16_t	Type;				// CPU type ('cpu-type') used in the SMBIOS patcher.
+		uint32_t	Features;			// CPU Features like MMX, SSE2, VT, MobileCPU
+		uint32_t	Vendor;				// Vendor
+		uint32_t	Signature;			// Signature
+		uint32_t	Stepping;			// Stepping
+		uint32_t	Model;				// Model
+		uint32_t	ExtModel;			// Extended Model
+		uint32_t	Family;				// Family
+		uint32_t	ExtFamily;			// Extended Family
+		uint32_t	NumCores;			// Number of cores per package
+		uint32_t	NumThreads;			// Number of threads per package
 
 #if USE_STATIC_CPU_DATA == 0 && DEBUG_CPU
-		uint8_t		CurrCoef;				// Current Multiplier (busratio).
-		uint8_t		MaxCoef;				// Max multiplier
+		uint8_t		CurrCoef;			// Current Multiplier (busratio).
+		uint8_t		MaxCoef;			// Max multiplier
 		uint8_t		CurrDiv;
 		uint8_t		MaxDiv;
 #endif
@@ -160,9 +160,9 @@ typedef struct _PlatformInfo_t
 		uint64_t	FSBFrequency;			// FSB Frequency Hz
 		uint64_t	CPUFrequency;			// CPU Frequency Hz
 
-		uint32_t	QPISpeed;				// QuickPath Interconnect Bus Speed
+		uint32_t	QPISpeed;			// QuickPath Interconnect Bus Speed
 
-		uint8_t		NumberOfTurboRatios;	// Jeroen: initialized in cpu.c and used in ACPI/ssdt_pr_generator.h
+		uint8_t		NumberOfTurboRatios;		// Jeroen: initialized in cpu.c and used in ACPI/ssdt_pr_generator.h
 
 		uint8_t		CoreTurboRatio[STATIC_CPU_NumCores]; // Used in cpu Intel/dynamic_data.h and ACPI/ssdt_pr_generator.h
 
@@ -170,71 +170,71 @@ typedef struct _PlatformInfo_t
 		uint8_t		MaxBusRatio;			// Used in cpu/Intel/dynamic_data.h and ACPI/ssdt_pr_generator.h
 
 #if AUTOMATIC_SSDT_PR_CREATION || DEBUG_CPU_TDP
-		uint8_t		TDP;					// Used in cpu/Intel/dynamic_data.h and ACPI/ssdt_pr_generator.h
+		uint8_t		TDP;				// Used in cpu/Intel/dynamic_data.h and ACPI/ssdt_pr_generator.h
 #endif
 		char		BrandString[48];		// Brand/frequency string
 		uint32_t	ID[MAX_CPUID_LEAVES][4];	// CPUID 0..4, 80..81 Raw Values
 	} CPU;
 
-    struct DMI								// Patch by: Asere / Rekursor.
+    struct DMI							// Patch by: Asere / Rekursor.
 	{
-		int				RAMSlotsPopulated;	// Number of memory slots populated by SMBIOS.
-		int				RAMSlotsSupported;	// Total number of on-board memory slots.
-		int				RAMSlotsActive;		// Total number of active slots (with module inserted).
-		int				MODULE[MAX_SLOTS];	// Information and SPD mapping for each slot.
+		int		RAMSlotsPopulated;		// Number of memory slots populated by SMBIOS.
+		int		RAMSlotsSupported;		// Total number of on-board memory slots.
+		int		RAMSlotsActive;			// Total number of active slots (with module inserted).
+		int		MODULE[MAX_SLOTS];		// Information and SPD mapping for each slot.
 	} DMI;
 
 #if INCLUDE_MPS_TABLE
-    struct MPS								// Multi Procesor Table
+    struct MPS							// Multi Procesor Table
 	{
-		EFI_GUID		Guid;
+		EFI_GUID	Guid;
 		
-		EFI_UINT		BaseAddress;		// Either 32 or 64-bit (depends on platform info).
+		EFI_UINT	BaseAddress;			// Either 32 or 64-bit (depends on platform info).
 	} MPS;
 #endif // INCLUDE_MP_TABLE
 
-    struct RAM								// Patch by: Asere / Rekursor.
+    struct RAM							// Patch by: Asere / Rekursor.
 	{
-		uint64_t		Frequency;			// RAM Frequency
-		uint32_t		Divider;			// Memory divider
-		uint8_t			CAS;				// CAS 1/2/2.5/3/4/5/6/7
-		uint8_t			TRC;
-		uint8_t			TRP;
-		uint8_t			RAS;
-		uint8_t			Channels;			// Channel Configuration Single, Dual or Triple
-		uint8_t			SlotCount;			// Previously NoSlots; - Maximum no of slots available
-		uint8_t			Type;				// Standard SMBIOS v2.5 Memory Type
+		uint64_t	Frequency;			// RAM Frequency
+		uint32_t	Divider;			// Memory divider
+		uint8_t		CAS;				// CAS 1/2/2.5/3/4/5/6/7
+		uint8_t		TRC;
+		uint8_t		TRP;
+		uint8_t		RAS;
+		uint8_t		Channels;			// Channel Configuration Single, Dual or Triple
+		uint8_t		SlotCount;			// Previously NoSlots; - Maximum no of slots available
+		uint8_t		Type;				// Standard SMBIOS v2.5 Memory Type
 		/* Remove me */
-	 	char *			BrandString;		// Branding String Memory Controller
-		RamSlotInfo_t	MODULE[MAX_SLOTS];	// Information about each slot
+	 	char *		BrandString;			// Branding String Memory Controller
+		RamSlotInfo_t	MODULE[MAX_SLOTS];		// Information about each slot
 	} RAM;
 
-    struct DT								// Device Tree
+    struct DT							// Device Tree
 	{
-			Node *	RootNode;				// Path: /
+			Node *	RootNode;			// Path: /
 	} DT;
 
     struct EFI
 	{
-		EFI_SYSTEM_TABLE *	SystemTable;	// Used to re-calculate the checksum at the end of the run.
+		EFI_SYSTEM_TABLE * SystemTable;			// Used to re-calculate the checksum at the end of the run.
 
 		struct Nodes
 		{
-			Node *	Chosen;					// Path: /chosen
-			Node *	MemoryMap;				// Path: /chosen/memory-map
+			Node *	Chosen;				// Path: /chosen
+			Node *	MemoryMap;			// Path: /chosen/memory-map
 
-											// EFI specific stuff.
+								// EFI specific stuff.
 			Node *	ConfigurationTable;		// Path: /efi/configuration-table
 			Node *	RuntimeServices;		// Path: /efi/runtime-services
-			Node *	Platform;				// Path: /efi/platform
+			Node *	Platform;			// Path: /efi/platform
 		} Nodes;
 	} EFI;
 
 	struct SMBIOS
 	{
-		EFI_GUID		Guid;
+		EFI_GUID	Guid;
 
-		EFI_UINT		BaseAddress;		// Either 32 or 64-bit (depends on platform info).
+		EFI_UINT	BaseAddress;		// Either 32 or 64-bit (depends on platform info).
 
 		/* UInt8	Anchor[4];				// DHP: Not used yet =]\._oOo_./[=
 		UInt8	Checksum;
